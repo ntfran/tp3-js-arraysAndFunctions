bdb60c7002497a90e82a48bf216e5aba20fad67d
ntfran <francisco.sandoval7700@gmail.com>
2026-06-30 05:26:50 -0300

    Hacer ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,9 +1,29 @@
-const paroimpar = (numero) => {
-  if (numero % 2 === 0) {
-    return "El numero es par";
-  } else {
-    return "El numero es impar";
+/* 1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
+ */
+const pantalla = document.getElementById("pantalla");
+
+const mostrarMeses = () => {
+  let lista = `<h2 class='my-3'>Meses del año</h2>`;
+  lista += '<ul class="list-group">';
+  for (let i = 0; i < meses.length; i++) {
+    lista += `<li class="list-group-item">${meses[i]}</li>`;
   }
+  lista += "</ul>";
+  pantalla.innerHTML += lista;
 };
-numero = parseInt(prompt("Ingresar un numero"));
-alert(paroimpar(numero));
+const meses = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+mostrarMeses();
